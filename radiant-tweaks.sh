82441e53f8e77fdf11dc205e613c71b488ab6e3a
fogrkan <fogrkan@gmail.com>
2021-05-01 18:23:01 +0300

    initial commit

---
+++ radiant-tweaks.sh
@@ -0,0 +1,22 @@
+RED="\e[31m"
+GREEN="\e[32m"
+ENDCOLOR="\e[0m"
+
+command=$(loginctl show-session $(loginctl | grep $(whoami) | awk '{print $1}') -p Type)
+if [ $command = 'Type=x11' ]; then
+	echo -e "${GREEN}Choose your bar theme:${ENDCOLOR}"
+	echo "blocks    colorblocks    cuts      docky
+forest    grayblocks     hack      material
+panels    pwidgets       shades    shapes
+default"
+	echo ""
+	read polybar_theme
+	if [ $polybar_theme = "default"  ]; then
+		killall -q polybar
+	        gnome-extensions disable hidetopbar@mathieu.bidon.ca
+	else
+	   gnome-extensions enable hidetopbar@mathieu.bidon.ca
+	   bash ~/.config/polybar/launch.sh --$polybar_theme
+	fi
+
+fi
